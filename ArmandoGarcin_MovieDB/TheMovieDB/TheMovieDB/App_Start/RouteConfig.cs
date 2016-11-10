7c067b2e147d53504b212f8a5739a84aff731db4
Armando Garcin <armandogarcin113@gmail.com>
2016-11-10 14:19:42 -0600

    MovieLib Page Added
    I added the movielib page, along with it's view and config. Please note,
    still no new methods yet, I'm setting up the pages first and then I'll
    tackle the other stuff.

--- ArmandoGarcin_MovieDB/TheMovieDB/TheMovieDB/App_Start/RouteConfig.cs
+++ ArmandoGarcin_MovieDB/TheMovieDB/TheMovieDB/App_Start/RouteConfig.cs
@@ -18,6 +18,9 @@ namespace TheMovieDB
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            //Register MovieLib page
+            routes.MapRoute("MovieLib", "MovieLib/{action}/{id}", new { controller = "MovieLib", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
---
+++ ArmandoGarcin_MovieDB/TheMovieDB/TheMovieDB/Controllers/MovieLibController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheMovieDB.Controllers
+{
+    public class MovieLibController : Controller
+    {
+        // GET: MovieLib
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MovieView()
+        {
+            return View();
+        }
+    }
+}
